a68edb36955cf03cbf9d361351c183ddfac2560f
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-05-02 11:42:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =========== typing animation ================*/
+var typed = new Typed(".typing",{
+    strings:["Web Designer", "Web App developer", "Internet of Things", "Software Engineer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
